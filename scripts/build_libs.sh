e42cb20914c5b47f9abad4cff533a3e10aadd5c3
Adrian Duke <adrian.duke@gmail.com>
2023-02-04 21:24:06 +0000

    updates

--- scripts/build_libs.sh
+++ scripts/build_libs.sh
@@ -1,14 +1,12 @@
-#! /bin/bash
-source build_settings
+#! /bin/bash -e
 
-grep 'docker\|lxc' /proc/1/cgroup > /dev/null 2>&1 || {
-    echo This script should only be called in a container. Consult the README for instructions
-    exit 1
-}
+source build_settings
+BBB_ADDRESS=root@$BBB_HOSTNAME
 
 # set date and build libraries
 ssh-keygen -R $1 &> /dev/null || true
 ssh -o StrictHostKeyChecking=no -o ConnectTimeout=5 $BBB_ADDRESS "date -s \"`date '+%Y%m%d %T %z'`\" > /dev/null"
 ssh $BBB_ADDRESS "cd Bela && rm lib/*"
 ssh $BBB_ADDRESS "cd Bela && make -f Makefile.libraries cleanall && make -f Makefile.libraries all"
-ssh $BBB_ADDRESS "cd Bela && make lib && make libbelafull"
+ssh $BBB_ADDRESS "cd Bela && make lib"
+ssh $BBB_ADDRESS "cd Bela && make libbelafull"
